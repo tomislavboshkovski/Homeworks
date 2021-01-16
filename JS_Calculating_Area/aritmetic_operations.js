78370003045681236836099073d4379387b9eaad
Tomislav Boshkovski <tomislavbosh@gmail.com>
2021-01-16 01:31:50 +0100

    An index.html document has been added

---
+++ JS_Calculating_Area/aritmetic_operations.js
@@ -0,0 +1,5 @@
+let radius = 10;
+let p = 3.14;
+let area = p * (radius * radius);
+
+document.getElementById("demo").innerHTML = `Answer&colon; The area of a circle with radius of 10 cm is ${area} cm.`
